e3c34b66fa980002979a74d6700b6bbba9adcbab
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-26 14:04:58 +0900

    初回コミット

---
+++ UITableViewSectionIndex/UITableViewSectionIndex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewSectionIndex
+//
+//  Created by home on 2020/02/26.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
