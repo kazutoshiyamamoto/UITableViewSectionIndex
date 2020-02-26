37ab3f5c2c6b249d21845784b77112377ad3c069
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-26 16:34:59 +0900

    索引の挙動チェックのためデータを追加

--- UITableViewSectionIndex/UITableViewSectionIndex/ViewController.swift
+++ UITableViewSectionIndex/UITableViewSectionIndex/ViewController.swift
@@ -23,7 +23,14 @@ class ViewController: UIViewController {
         List(listID: 3, listName1: "テント", listName2: "tent"),
         List(listID: 10, listName1: "リンゴ", listName2: "apple"),
         List(listID: 12, listName1: "オレンジ", listName2: "orange"),
-        List(listID: 5, listName1: "サカナ", listName2: "fish")
+        List(listID: 5, listName1: "サカナ", listName2: "fish"),
+        List(listID: 13, listName1: "ピーチ", listName2: "peach"),
+        List(listID: 17, listName1: "グレープ", listName2: "grape"),
+        List(listID: 19, listName1: "ナシ", listName2: "pear"),
+        List(listID: 21, listName1: "パイナップル", listName2: "pineapple"),
+        List(listID: 34, listName1: "マンゴー", listName2: "mango"),
+        List(listID: 45, listName1: "ユズ", listName2: "citron"),
+        List(listID: 2, listName1: "ライチ", listName2: "litchi")
     ]
     
     private var sortedList = [(key: String, value: [List])]()
